c2c5784dd2586948f7427ebfffc0ca0fd617f59e
ashna <ashnakarim2024@mca.ajce.in>
2025-05-09 21:15:46 +0530

    Initial commit for product management API

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const productRoutes = require('./routes/productRoutes');
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json())
+
+// Routes
+app.use('/api/products', productRoutes);
+
+// Database Connection
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running on http://localhost:${process.env.PORT}`);
+    });
+  })
+  .catch((err) => console.log('DB connection error:', err));
